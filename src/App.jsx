9a9f52e4db8e4de47fd41724d17b7e16a8f0cc56
leontoys <svliyon@gmail.com>
2025-03-16 11:51:42 +1100

    code-clean-up-done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import "./styles.css"
 import { useState, useEffect } from 'react';
 import MovieList from '../../src/components/MovieList';
-import "./styles.css"
 import Heading from './components/Heading';
 import Search from './components/Search';
 import AddFavourites from './components/AddFavourites';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
@@ -15,14 +14,21 @@ function App() {
   //query OMDB
   const getMovies = async (searchValue) => {
     try {
-      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a2afdd9f`
+      //read api key
+      const apikey = import.meta.env.VITE_OMDB_API_KEY
+      //omdb query 
+      const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${apikey}`
       const response = await fetch(url)
+      console.log("response",response)
+      //if response has any error
       if (!response.ok) {
         throw new Error("OMDB Error")
       }
       const responseJson = await response.json()
+      console.log("responsejson",responseJson)
+      //if any movies found, only then update -- to avoid null error
       if (responseJson.Search) {
-        console.log(responseJson.Search)
+        console.log("movies",responseJson.Search)
         setMovies(responseJson.Search)
       }
     } catch (error) {
@@ -30,26 +36,34 @@ function App() {
     }
   }
 
-  //on page load - read from local storage
+  //Only on page initial load - read from local storage
   useEffect(()=>{
+    //check if user has saved any favourites - otherwise empty array to avoid null error
     const newList =  JSON.parse(localStorage.getItem("react-movie-favourites"))||[]
+    console.log('initial-load-local-storage',newList)
+    //set the local favourites
     setFavourites(newList)
   },[])
 
   //as soon as search value changes query
   useEffect(() => {
+    console.log("search query",searchValue)
     getMovies(searchValue)
   }, [searchValue])
 
+  //add
   const addToFavourites = (movie)=>{
     //update favourites list
     const newList = [...favourites,movie]
+    console.log("add-favourites",newList)
     setFavourites(newList)
     localStorage.setItem("react-movie-favourites",JSON.stringify(newList))
   }
 
+  //remove
   const removeFromFavourites = (movie)=>{
     const newList = favourites.filter(item=>item.imdbID!==movie.imdbID)
+    console.log("remove-favourites",newList)
     setFavourites(newList)
     localStorage.setItem("react-movie-favourites",JSON.stringify(newList))
   }
@@ -68,9 +82,9 @@ function App() {
           AddFavourites={AddFavourites}></MovieList>
       </div>
       <div className='favourites'>
-      <Heading heading="Favourites"></Heading>
-      <div className='list-container'>
-        <MovieList movies={favourites}
+        <Heading heading="Favourites"></Heading>
+        <div className='list-container'>
+          <MovieList movies={favourites}
                    handleClick={removeFromFavourites}
           AddFavourites={RemoveFavourites}></MovieList>
       </div>
