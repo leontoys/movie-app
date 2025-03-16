e9c93e4acc27e0203f043344b8a09098c2a16b69
leontoys <svliyon@gmail.com>
2025-03-16 11:26:12 +1100

    remove-from-favourites-work

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,23 @@ import Heading from './components/Heading';
 import Search from './components/Search';
 import AddFavourites from './components/AddFavourites';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import RemoveFavourites from './components/RemoveFavourites';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [searchValue,setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("")
+  const [favourites, setFavourites] = useState([])
 
   //query OMDB
   const getMovies = async (searchValue) => {
     try {
       const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=a2afdd9f`
       const response = await fetch(url)
-      if(!response.ok){
+      if (!response.ok) {
         throw new Error("OMDB Error")
       }
       const responseJson = await response.json()
-      if(responseJson.Search){
+      if (responseJson.Search) {
         console.log(responseJson.Search)
         setMovies(responseJson.Search)
       }
@@ -28,20 +30,42 @@ function App() {
     }
   }
 
-  //on page load
+  //as soon as search value changes query
   useEffect(() => {
     getMovies(searchValue)
   }, [searchValue])
+
+  const addToFavourites = (movie)=>{
+    //update favourites list
+    setFavourites([...favourites,movie])
+    console.log(favourites)
+  }
+
+  const removeFromFavourites = (movie)=>{
+    const newList = favourites.filter(item=>item.imdbID!==movie.imdbID)
+    setFavourites(newList)
+  }
 
   return (
     <div className='container'>
       <h1>REACT FLIX</h1>
       <div className='header'>
         <Heading heading="Movies"></Heading>
-        <Search searchValue={searchValue} setSearchValue={setSearchValue}></Search>
+        <Search searchValue={searchValue}
+          setSearchValue={setSearchValue}></Search>
       </div>
       <div className='list-container'>
-        <MovieList movies={movies} AddFavourites={AddFavourites}></MovieList>
+        <MovieList movies={movies}
+                   handleClick={addToFavourites}
+          AddFavourites={AddFavourites}></MovieList>
+      </div>
+      <div className='favourites'>
+      <Heading heading="Favourites"></Heading>
+      <div className='list-container'>
+        <MovieList movies={favourites}
+                   handleClick={removeFromFavourites}
+          AddFavourites={RemoveFavourites}></MovieList>
+      </div>
       </div>
     </div>
   );
---
+++ src/components/RemoveFavourites.jsx
@@ -0,0 +1,9 @@
+const RemoveFavourites = ()=>{
+    return(
+        <div className="overlay">
+            Remove from Favourites
+        </div>
+    )
+}
+
+export default RemoveFavourites
