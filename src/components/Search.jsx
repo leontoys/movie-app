62c2d69f1c0c3360947ca6b899cc99af2e66ee2c
leontoys <svliyon@gmail.com>
2025-03-17 18:26:27 +1100

    changed to on input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({ searchValue, setSearchValue }) => {
                 inputMode="text"
                 autoComplete="off"
                 value={searchValue}
-                onKeyUp={(e) => {
+                onInput={(e) => {
                     console.log("change fired", e.target.value);
                     setSearchValue(e.target.value);
                 }}
